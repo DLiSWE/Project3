ad0a148d1d59566c001a2c3b1d2c10c32bdd5abe
Daywick <derekli11204@gmail.com>
2022-02-26 16:57:28 -0500

    number check works

--- proj3/proj3script.js
+++ proj3/proj3script.js
@@ -6,8 +6,12 @@ const generateNumber = () => {
     return Math.trunc(Math.random()*100+1);
 }
 
+
 // get random choice
 const randomchoice = generateNumber();
+
+console.log(randomchoice)
+
 
 // logic to check whether user input is number
 document.getElementById("submit1").addEventListener('click', function(){
@@ -15,21 +19,30 @@ document.getElementById("submit1").addEventListener('click', function(){
     if (Number(uGuess) == randomchoice){
         document.getElementById('phrase').innerHTML = `${uGuess} is right!`;
         document.getElementById('high').innerHTML = `${score} `;
+        document.body.style.backgroundColor = 'green'
+        document.getElementById('main').style.backgroundColor = 'lightgreen';
     }
     else if (Number(uGuess) < randomchoice){
         document.getElementById('phrase').innerHTML = `Your guess is too low`;
         score--;
         document.getElementById('current').innerHTML = score
+        if (Number(document.getElementById('current').innerText) == 0){
+            document.body.style.backgroundColor = 'red'
+            document.getElementById('main').style.backgroundColor = 'pink';
+        }
     }
     else if (Number(uGuess) > randomchoice){
         document.getElementById('phrase').innerHTML = `Your guess is too high`;
         score--;
         document.getElementById('current').innerHTML = score
+        if (Number(document.getElementById('current').innerText) == 0){
+            console.log(document.getElementById('current').innerText)
+            document.body.style.backgroundColor = 'red'
+            document.getElementById('main').style.backgroundColor = 'pink';
+        }
     }
     else{
         document.getElementById('phrase').innerHTML = 'Your guess is not within 1-100. Please try again';
     }
 })
-
-// Check if score is 0
 
